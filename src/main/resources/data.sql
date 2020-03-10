a8f9490c9756b619ff6efd4fce3e3cb9c7c2f139
AymenKarouia <aymen.karouia24@gmail.com>
2020-03-10 20:17:38 +0100

    version 1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into Employees values(8888 , 49, 43, 73, 53 );
+insert into Employees values(7777 , 90, 44, 135, 382 ); 
+insert into Employees values(8877 , 89, 44, 102, 137 ); 
+insert into Employees values(7788 , 89, 44, 106, 392 ); 
